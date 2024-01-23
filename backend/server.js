c397d35f5f6e223a5ce230ba57a102e626cfbf20
codeabuu <abdulngui718@gmail.com>
2024-01-23 16:23:36 -0500

    update middleware

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,22 @@
+require('dotenv').config()
+
 const express = require('express')
 
 //express app
 const app = express()
 
+//middleware,access to req and res
+app.use((req, res, next) => {
+  console.log(req.path, req.method)
+  next()
+})
+
+//route handler
+app.get('/', (req, res) => {
+  res.json({mssg: "Welcome to the app"})
+})
+
 //listen for requests
-app.listen(4000, () => {
+app.listen(process.env.PORT, () => {
   console.log('listening to port 4000')
 })
