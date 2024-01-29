9bc5e5e1ee059fe375c892934422484e83a27aa4
codeabuu <abdulngui718@gmail.com>
2024-01-29 07:24:36 -0500

    frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+//pages and components
+import Home from "./pages/Home"
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <div className="pages">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+}
+export default App;
