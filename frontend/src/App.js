22f49e663a1cdde7418737f615f09cd77863fab3
codeabuu <abdulngui718@gmail.com>
2024-01-29 08:26:09 -0500

    css with home and tile page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,11 +2,13 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 //pages and components
 import Home from "./pages/Home"
+import Navbar from "./components/Navbar"
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+	<Navbar />
         <div className="pages">
           <Routes>
             <Route path="/" element={<Home />} />
