a2fb366752b5f6cc6fd9b834eddfdc4a1d9b604f
codeabuu <abdulngui718@gmail.com>
2024-02-03 15:29:26 -0500

    context

---
+++ frontend/src/context/workoutContext.js
@@ -0,0 +1,31 @@
+import {createContext, useReducer} from 'react'
+
+export const WorkoutContext = createContext()
+
+export const workoutsReducer = (state, action) => {
+  switch (action.type) {
+    case 'SET_WORKOUTS':
+      return {
+	workouts: action.payload
+      }
+    case 'CREATE_WORKOUT':
+      return {
+	workouts: [action.payload, ...state.workouts]
+      }
+    default:
+      return state
+  }
+}
+
+export const WorkoutContextProvider = ({children}) => {
+  const [state, dispatch] = useReducer(workoutsReducer, {
+    workouts: null
+  })
+  
+  return (
+    <WorkoutContext.Provider value={{state, dispatch}}>
+	  {children}
+
+    </WorkoutContext.Provider>
+  )
+}
