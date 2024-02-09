1cd6d612e249fb20dfffcd9acd419c4ad876d901
codeabuu <abdulngui718@gmail.com>
2024-02-09 16:20:15 -0500

    pass and email validator, bscrypt, salt

--- backend/models/userMod.js
+++ backend/models/userMod.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const bcrypt =require('bcrypt')
+const validator = require('validator')
 
 const userSchema = new Schema({
   email: {
@@ -15,6 +16,16 @@ const userSchema = new Schema({
 })
 //static sign up
 userSchema.statics.signup = async function(email, password) {
+  //validation
+  if (!email || !password) {
+    throw Error("Please fill all fields")
+  }
+  if (!validator.isEmail(email)) {
+    throw Error('Email not valid')
+  }
+  if (!validator.isStrongPassword(password)) {
+    throw Error('Password not strong enough')
+  }
   const exists = await this.findOne({email})
   if (exists) {
     throw Error('Email already in Use')
